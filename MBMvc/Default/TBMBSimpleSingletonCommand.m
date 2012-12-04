17074777bc42b3c6b4d4ad58d74455615bceffbe
wentong <wentong@taobao.com>
2012-12-04 14:54:22 +0800

    修正单例command 穿错的bug

--- MBMvc/Default/TBMBSimpleSingletonCommand.m
+++ MBMvc/Default/TBMBSimpleSingletonCommand.m
@@ -3,21 +3,39 @@
 //
 
 
+#import <pthread.h>
 #import "TBMBSimpleSingletonCommand.h"
 
+static NSMutableDictionary *_commandInstanceMap = nil;
+static dispatch_once_t _oncePredicate_CommandInstanceMap;
+
+static pthread_rwlock_t _lock;
 
 @implementation TBMBSimpleSingletonCommand
 + (TBMBSimpleSingletonCommand *)instance {
-    static TBMBSimpleSingletonCommand *_instance = nil;
-    static dispatch_once_t _oncePredicate_TBMBSimpleSingletonCommand;
-
-    dispatch_once(&_oncePredicate_TBMBSimpleSingletonCommand, ^{
-        if (_instance == nil) {
-            _instance = [[self alloc] init];
+    NSString *className = NSStringFromClass(self);
+    dispatch_once(&_oncePredicate_CommandInstanceMap, ^{
+        pthread_rwlock_init(&_lock, NULL);
+        if (_commandInstanceMap == nil) {
+            _commandInstanceMap = [[NSMutableDictionary alloc] init];
         }
     }
     );
-
-    return _instance;
+    TBMBSimpleSingletonCommand *_instance = nil;
+    pthread_rwlock_rdlock(&_lock);
+    _instance = [_commandInstanceMap objectForKey:className];
+    pthread_rwlock_unlock(&_lock);
+    if (_instance) {
+        return _instance;
+    } else {
+        pthread_rwlock_wrlock(&_lock);
+        _instance = [_commandInstanceMap objectForKey:className];
+        if (!_instance) {
+            _instance = [[self alloc] init];
+            [_commandInstanceMap setObject:_instance forKey:className];
+        }
+        pthread_rwlock_unlock(&_lock);
+        return _instance;
+    }
 }
 @end
