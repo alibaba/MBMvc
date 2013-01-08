ad9a2b8ccd15a5a3dc9e836bda4b8ec4904ceac6
wentong <wentong@taobao.com>
2013-01-08 13:48:31 +0800

    准备支持No-ARC

--- MBMvc/Util/TBMBUtil.h
+++ MBMvc/Util/TBMBUtil.h
@@ -12,6 +12,35 @@
 #define TBMB_LOG(msg, args...)
 #endif
 
+
+#if !__has_feature(objc_arc)
+#define TBMBAutoRelease(__v) ([__v autorelease]);
+#define TBMBReturnAutoReleased TBMBAutoRelease
+
+#define TBMBRetain(__v) ([__v retain]);
+#define TBMBReturnRetained TBMBRetain
+
+#define TBMBRelease(__v) ([__v release]);
+#define TBMBSafeRelease(__v) ([__v release], __v = nil);
+#define TBMBSuperDealloc [super dealloc];
+
+#define TBMBWeak
+#else
+// -fobjc-arc
+#define TBMBAutoRelease(__v)
+#define TBMBReturnAutoReleased(__v) (__v)
+
+#define TBMBRetain(__v)
+#define TBMBReturnRetained(__v) (__v)
+
+#define TBMBRelease(__v)
+#define TBMBSafeRelease(__v) (__v = nil);
+#define TBMBSuperDealloc
+
+#define TBMBWeak __unsafe_unretained
+#endif
+
+
 #define TBMB_PROXY_PREFIX @"__##__ProxyHandler"
 
 @class TBMBDefaultMessageReceiver;
