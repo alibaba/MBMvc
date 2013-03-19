8158617232600f64670265197db6dba25adb916d
wentong <wentong@taobao.com>
2013-03-19 09:06:11 +0800

    没时间支持No-Arc

--- MBMvc/Util/TBMBUtil.h
+++ MBMvc/Util/TBMBUtil.h
@@ -22,32 +22,32 @@
 #endif
 
 
-#if !__has_feature(objc_arc)
-#define TBMBAutoRelease(__v) ([__v autorelease]);
-#define TBMBReturnAutoReleased TBMBAutoRelease
-
-#define TBMBRetain(__v) ([__v retain]);
-#define TBMBReturnRetained TBMBRetain
-
-#define TBMBRelease(__v) ([__v release]);
-#define TBMBSafeRelease(__v) ([__v release], __v = nil);
-#define TBMBSuperDealloc [super dealloc];
-
-#define TBMBWeak
-#else
-// -fobjc-arc
-#define TBMBAutoRelease(__v)
-#define TBMBReturnAutoReleased(__v) (__v)
-
-#define TBMBRetain(__v)
-#define TBMBReturnRetained(__v) (__v)
-
-#define TBMBRelease(__v)
-#define TBMBSafeRelease(__v) (__v = nil);
-#define TBMBSuperDealloc
-
-#define TBMBWeak __unsafe_unretained
-#endif
+//#if !__has_feature(objc_arc)
+//#define TBMBAutoRelease(__v) ([__v autorelease]);
+//#define TBMBReturnAutoReleased TBMBAutoRelease
+//
+//#define TBMBRetain(__v) ([__v retain]);
+//#define TBMBReturnRetained TBMBRetain
+//
+//#define TBMBRelease(__v) ([__v release]);
+//#define TBMBSafeRelease(__v) ([__v release], __v = nil);
+//#define TBMBSuperDealloc [super dealloc];
+//
+//#define TBMBWeak
+//#else
+//// -fobjc-arc
+//#define TBMBAutoRelease(__v)
+//#define TBMBReturnAutoReleased(__v) (__v)
+//
+//#define TBMBRetain(__v)
+//#define TBMBReturnRetained(__v) (__v)
+//
+//#define TBMBRelease(__v)
+//#define TBMBSafeRelease(__v) (__v = nil);
+//#define TBMBSuperDealloc
+//
+//#define TBMBWeak __unsafe_unretained
+//#endif
 
 
 #define TBMB_PROXY_PREFIX @"__##__ProxyHandler"
